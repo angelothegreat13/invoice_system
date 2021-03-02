269ad414a929d4cd382861a5c35d6bc35ab44a82
Michael Angelo Manaog <angelothegreat13@gmail.com>
2021-03-03 06:52:56 +0800

    test 2

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,11 +11,6 @@ class HomeController extends Controller
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
         return view('home');
